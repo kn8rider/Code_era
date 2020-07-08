3ad688982dcc3015941731f787438a20c66a9afd
kn8rider <khemchandpatel784@gmail.com>
2020-07-08 18:27:53 +0530

    first commit

---
+++ precache-manifest.19d533d467d8834745a689405512a48a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca55c0f1aa57d0b42fce39f531d24c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b986d4bcdda9219afb9",
+    "url": "./static/css/main.9153cbde.chunk.css"
+  },
+  {
+    "revision": "1747dcbe2525bceaf352",
+    "url": "./static/js/2.f28647be.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.f28647be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b986d4bcdda9219afb9",
+    "url": "./static/js/main.b8784ddc.chunk.js"
+  },
+  {
+    "revision": "5a5f4633800699d7383c",
+    "url": "./static/js/runtime-main.0d2694b0.js"
+  }
+]);
